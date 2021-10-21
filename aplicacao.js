c64112ddf01bdcd5ddc8d9b7ffc320b5d0e350d2
Michelle Pereira <michelle08082015@gmail.com>
2021-10-20 21:10:10 -0300

    criado as rotas post, put e delete

--- aplicacao.js
+++ aplicacao.js
@@ -1,6 +1,8 @@
 var app = require('express'); //importando a lib express
 var mysqlImporte = require('mysql'); //importando a lib que se conecta ao banco de dados
 var appExe = app(); //executando a função
+appExe.use(app.json());//método do express para efetuar a leitura do corpo da requisição (body)
+
 
 appExe.get('/rotaTeste', function(req, res) { //criando rota com método GET (pegar)
     console.log("A rota foi criada"); //exibe no terminal a mensagem servindo para teste
@@ -19,6 +21,42 @@ appExe.get('/noticias', function(req, res) { //criando rota com método Get para
     });
 });
 
+
+appExe.post('/noticias', function(req, res) {//criando rota com método Post para noticias
+    var conexaoBanco = mysqlImporte.createConnection({
+        host : "localhost",
+        user : "root",
+        password : "1234",
+        database : "portal_noticias"
+    });
+    conexaoBanco.query(`insert into noticias (titulo, noticia) values ('${req.body.titulo}', '${req.body.noticia}')`, function(error, result) {//template literals concatenando com string externa enviada pelo usuário
+        res.send(result)
+    });
+});
+
+appExe.put('/noticias/:id', function(req, res) {
+    var conexaoBanco = mysqlImporte.createConnection({
+        host : "localhost",
+        user : "root",
+        password : "1234",
+        database : "portal_noticias"
+    });
+    conexaoBanco.query(`update noticias set titulo = '${req.body.titulo}', noticia = '${req.body.noticia}' where id_noticia = '${req.params.id}' `, function(error, result) {
+        res.send(result)
+    });
+});
+
+appExe.delete('/noticias/:id', function(req, res) {
+    var conexaoBanco = mysqlImporte.createConnection({
+        host : "localhost",
+        user : "root",
+        password : "1234",
+        database : "portal_noticias"
+    });
+    conexaoBanco.query(`delete from noticias where id_noticia =  '${req.params.id}'`, function(error, result) {
+        res.send(result)
+    });
+});
 
 appExe.listen(3000, function() { //método listen que "cria" a porta do servidor
     console.log("A porta é 3000"); //verificando se funcionou corretamente
